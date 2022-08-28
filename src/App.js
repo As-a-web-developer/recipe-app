a63db39640ca523535e685ddf5b63cc27c7893ad
Adesipe Adeola <princeadeola234@gmail.com>
2022-08-28 12:44:28 -0700

    committed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, {useEffect} from 'react';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = "5cde7a8d";
   const APP_KEY = "9f4c0ea907393126b86293f1404a814e";
+
+  useEffect( () => {
+    getRecipes();
+  }, []);
+ 
+  const getRecipes = async () => {
+    //const response = await fetch(`https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}&type=public`);
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data.hits);
+  }
+
   return(
     <div className='App'>
       <form className='search-form'>
