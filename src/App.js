06795739102293eeb1facbb8ecc103a89b9ce88e
Adesipe Adeola <princeadeola234@gmail.com>
2022-08-29 09:01:34 -0700

    committed

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
   }
 
   return(
@@ -26,6 +27,17 @@ const App = () => {
         <input className='search-bar' type="text" />
         <button className='search-button' type='submit'>Search</button>
       </form>
+
+      { 
+        recipes.map(recipe => (
+          <Recipe 
+                  key={recipe.recipe.label} 
+                  title={recipe.recipe.label} 
+                  calories={recipe.recipe.calories}
+                  image={recipe.recipe.image}
+          />
+        ))
+      };
     </div>
   );
 }
